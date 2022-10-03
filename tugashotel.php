217f171d956b13d1f3238d0fde472ace92c9d895
sifakhoirulmuna29 <109189084+sifakhoirulmuna29@users.noreply.github.com>
2022-10-03 08:50:34 +0700

    Add files via upload

---
+++ tugashotel.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTH-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device, initial-scale=1.0">
+    <title >Hotel Hebat </title>
+    <link rel="stylesheet" href="style.css" >
+</head>
+<body>
+    
+            <ul>
+                <li><a href="fasilitas.html">Fasilitas</a></li>
+                <li><a href="kamar.html">Kamar</a></li>
+                <li><a href="index.html">Home</a></li>
+                <li style="float: left;" class="active"><h2>HOTEL HEBAT</h2></li>
+            </ul>
+            <div class="header">
+                <h1>Selamat Datang Di Hotel Hebat!</h1>
+                <p>Hotel Mewah Dengan Harga Merakyat</p>
+        </div>
+        
+      
+</body>
+
+</html>
